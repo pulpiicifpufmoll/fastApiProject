a75038e6f6e106c21ab04082574c500271ff61a3
David Acedo <davidacedo9@gmail.com>
2023-11-24 11:37:42 +0100

    cors

--- main.py
+++ main.py
@@ -17,12 +17,20 @@
 import requests
 
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #Carpeta estática habilitada para archivos estáticos como el HTML o CSS
 app.mount("/static", StaticFiles(directory="static"), name="static")
